cc65d32767e774f06c62dec145b6ca09d956997a
Roberto-Carlos <robertocarlos12345@gmail.com>
2025-11-11 10:16:56 -0400

    Variables de entorno y Config DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env'
+    }),
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: +'${process.env.DATABASE_PORT}' || 5432,
+        username: process.env.DATABASE_USER || 'postgres',
+        password: process.env.DATABASE_PASSWORD || 'mi-123456',
+        database: process.env.DATABASE_NAME || 'inventario_back_nest',
+        entities: [
+          __dirname + '/../**/*.entity{.ts,.js}'
+        ],
+        synchronize: false,
+      }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
